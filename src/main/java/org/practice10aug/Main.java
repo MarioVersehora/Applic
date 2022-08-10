4ea686bdeb7122d213d11b1aae4b729b8a676aeb
MarioVersehora <marioalbert_o@yahoo.com>
2022-08-10 11:41:11 +0300

    Initial commit

---
+++ src/main/java/org/practice10aug/Main.java
@@ -0,0 +1,5 @@
+package org.practice10aug;public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/java/org/practice10aug/User.java
@@ -0,0 +1,2 @@
+package org.practice10aug;public class uSER {
+}
